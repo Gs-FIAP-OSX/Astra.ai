333fe0fe66d676d674d24df474d5f5a174f978fa
leeosilvp <leo965679@gmail.com>
2026-05-28 18:25:32 -0300

    feat: criando função para fechar o Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,14 +1,39 @@
 import '../css/aside.css'
+import { useEffect, useState } from 'react'
 import logo from '../assets/svg/logo.svg'
 import { Sidebar } from '@geist-ui/icons'
 
 const Aside = () => {
-    
+
+    const [isOpen, setIsOpen] = useState(() => {
+        return localStorage.getItem('aside_open') !== 'false'
+    })
+
+    useEffect(() => {
+        const handleAsideToggle = () => {
+            setIsOpen(localStorage.getItem('aside_open') !== 'false')
+        }
+
+        window.addEventListener('aside-toggle', handleAsideToggle)
+
+        return () => {
+            window.removeEventListener('aside-toggle', handleAsideToggle)
+        }
+    }, [])
+
+    const handleToggleAside = () => {
+        const newValue = !isOpen
+
+        localStorage.setItem('aside_open', newValue.toString())
+
+        window.dispatchEvent(new Event('aside-toggle'))
+    }
+
     return (
-        <aside className="aside-main">
+        <aside className={`aside-main ${isOpen ? 'collapsed' : ''}`}>
             <header className="aside-header">
                 <img src={logo} alt="logo ASTRA" />
-                <button><Sidebar size={17} /> </button>
+                <button onClick={handleToggleAside}><Sidebar size={17} /> </button>
             </header>
         </aside>
     )
