be4da8143ff4eed9163587ad35539e4099ef1e8b
leeosilvp <leo965679@gmail.com>
2026-05-28 21:26:59 -0300

    feat: criando estrutura do conteudo de Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,7 +1,7 @@
 import '../css/aside.css'
 import { useEffect, useState } from 'react'
 import logo from '../assets/svg/logo.svg'
-import { Sidebar } from '@geist-ui/icons'
+import { Code, Edit, Folder, Search, Sidebar, Trash2 } from '@geist-ui/icons'
 
 const Aside = () => {
 
@@ -35,6 +35,25 @@ const Aside = () => {
                 <img src={logo} alt="logo ASTRA" />
                 <button onClick={handleToggleAside}><Sidebar size={17} /> </button>
             </header>
+            <section className='aside-content'>
+                <button className='aside-btn'><Edit className='icon' size={19} /> <p>Novo bate-papo</p> <span>Shift + a</span></button>
+                <button className='aside-btn'><Search className='icon' size={19} /> <p>Procurar</p> <span>Ctrl + p</span></button>
+                <div className='aside-content-scrool'>
+                    <button className='aside-btn'><Folder className='icon' size={19} /> <p>Arquivos</p></button>
+                    <button className='aside-btn bloq'><Code className='icon' size={19} /> <p>Código</p> <h3>Indisponível</h3></button>
+
+                    <h2>Recentes</h2>
+                    <section className='aside-grid-chats'>
+                        <article className='aside-card-chat'>
+                            <h1>Novo Chat</h1>
+                            <div>
+                                <button><Trash2 size={14} /></button>
+                                <button><Edit size={14} /></button>
+                            </div>
+                        </article>
+                    </section>
+                </div>
+            </section>
         </aside>
     )
 }
