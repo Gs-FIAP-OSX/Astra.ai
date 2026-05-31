66384c1bd30144dc139cc31a3717c14c65036d33
leeosilvp <leo965679@gmail.com>
2026-05-31 18:50:47 -0300

    feat: criando rota Error

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,9 +1,14 @@
 import '../css/error.css'
+import logo from '../assets/svg/logo.svg'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
         <main className='error-main'>
-
+            <img src={logo} />
+            <h1>Página não encontrada</h1>
+            <p>O universo é vasto, mas este endereço não foi encontrado.</p>
+            <Link to='/new'>Voltar para a página inicial</Link>
         </main>
     )
 }
